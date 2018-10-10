a32c38eceb6b63ad460b1b4b23276b7eecb4dac3
JiaXun <2583476396@qq.com>
2018-10-10 11:20:06 +0800

    初次使用idea搭建springboot

---
+++ src/main/java/com/ml/springboot/demo/SpringbootstudyApplication.java
@@ -0,0 +1,20 @@
+package com.ml.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootstudyApplication {
+
+    @RequestMapping("/")
+    String index( ){
+        return "hello spring boot";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootstudyApplication.class, args);
+    }
+}
